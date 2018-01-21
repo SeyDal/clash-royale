0c1065c04aa6ba7cd7f7fa1fd8405c4e1188212b
daddy-s-home <mehdi6583@gmail.com>
2018-01-21 15:07:52 +0330

    heros completed 1

--- clash royale.py
+++ clash royale.py
@@ -1,5 +1,5 @@
 import pygame
-class hero :
+class Hero :
     def __init__(self,damage,hit_point,hit_speed,range,area_damage,hero_cost,hero_type,target):
         self.Damage=damage
         self.hit_point=hit_point
@@ -9,9 +9,31 @@ class hero :
         self.hero_cost=hero_cost
         self.type=hero_type
         self.target=target
-
-class wizard(hero):
+#heros infornarions
+class Wizard(Hero):
     def __init__(self,position):
-        hero.__init__(self,228,598,1.4,5.5,True,4,"ground",("air","ground","building"))
+        Heroero.__init__(self,228,598,1.4,5.5,True,4,"ground",("air","ground","building"))
         self.position = position
+
+class Ballon(Hero):
+    def __init__(self,position):
+        Hero.__init__(self,798,1396,3,1,False,5,"air",("building"))
+        self.position=position
+
+class Pekka(Hero):
+    def __init__(self,position):
+        Hero.__init__(self,598,1059,1.8,1,False,4,"ground",("ground","building"))
+        self.position=position
+
+class Archer(Hero):
+    def __init__(self,position):
+        Hero.__init__(self,86,254,1.2,2,True,2,"ground",("air","ground","building"))
+        self.position=position
+
+class Power :
+    def __init__(self):
+        self.radius=5
+        self.duration=7.5
+        self.boost=35
+        self.hero_cost=3
         
