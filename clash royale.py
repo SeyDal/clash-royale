ea78fd032ee912437e0ade97e420ceec47262f4e
daddy-s-home <mehdi6583@gmail.com>
2018-01-22 16:27:35 +0330

    building class

--- clash royale.py
+++ clash royale.py
@@ -1,4 +1,6 @@
-import pygame
+import pygame,time,random,sys
+import pygame.locals as GAME_GLOBALS
+import pygame.event as GAME_EVENTS
 class Hero :
     def __init__(self,damage,hit_point,hit_speed,range,area_damage,hero_cost,hero_type,target):
         self.Damage=damage
@@ -9,6 +11,7 @@ class Hero :
         self.hero_cost=hero_cost
         self.type=hero_type
         self.target=target
+
 #heros infornarions
 class Wizard(Hero):
     def __init__(self,position):
@@ -44,17 +47,38 @@ class Power :
 
 class Giant(Hero):
     def __init__(self, position):
-        hero.__init__(self, 211, 3344, 1.5, 1 , False, 5, "ground", ("building"))
+        Hero.__init__(self, 211, 3344, 1.5, 1 , False, 5, "ground", ("building"))
         self.position = position
 
 
 class Barbar(Hero):
     def __init__(self, position):
-        hero.__init__(self, 155, 636, 1.5, 1, False, 2, "ground", ("ground", "building"))
+        Hero.__init__(self, 155, 636, 1.5, 1, False, 2, "ground", ("ground", "building"))
         self.position = position
 
 
 class Megaminion(Hero):
     def __init__(self, position):
-        hero.__init__(self, 258, 695, 1.5, 2, True, 3, "air", ("air","ground", "building"))
+        Hero.__init__(self, 258, 695, 1.5, 2, True, 3, "air", ("air","ground", "building"))
         self.position = position
+
+#builging information
+class Building :
+    def __init__(self,damage,hit_point,hit_speed,range):
+        self.damage=damage
+        self.hit_point=hit_point
+        self.hit_speed=hit_speed
+        self.range=range
+
+class King_tower (Building):
+    def __init__(self,position):
+        Building.__init__(self,90,4500,1,7)
+        self.position=position
+
+class Princess_tower (Building):
+    def __init__(self,position):
+        Building.__init__(self,100,2800,0.8,7.5)
+        self.position=position
+
+
+
